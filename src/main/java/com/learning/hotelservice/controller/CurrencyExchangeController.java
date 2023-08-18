6306e4ea685d3a12c06f941f70cb9c5b9cb749ab
raviipawar <ravibalajipawar@gmail.com>
2023-08-18 18:03:47 +0530

    cirbuit breaker impl

--- src/main/java/com/learning/hotelservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/learning/hotelservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.learning.hotelservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +17,22 @@ import com.learning.hotelservice.services.CurrencyExchangeService;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private CurrencyExchangeService currencyExchangeService;
 	
 	@PostMapping("/add")
 	public ResponseEntity<ExchangeValue> createHotel(@RequestBody ExchangeValue exchangeValue) {
 		ExchangeValue exchangeValue1 = currencyExchangeService.saveExchangeValue(exchangeValue);
+		
 		return ResponseEntity.status(HttpStatus.CREATED).body(exchangeValue1);
 	}
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchanges/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchangeValue = currencyExchangeService.findByFromAndTo(from, to);
+		logger.info("retrieveExchangeValue method callled with {}  to {}", from , to);
 		return exchangeValue;
 	}
 }
